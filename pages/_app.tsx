54e17f8d449514a26a6dcc2a529b7a1911a51693
Mat <matiastejerina94@gmail.com>
2023-08-08 18:09:36 -0300

    theme default dark

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,12 @@
 import '@/styles/globals.css'
+import { darkTheme } from '@/themes'
 import { ThemeProvider } from '@mui/material'
 
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={ }>
+    <ThemeProvider theme={darkTheme}>
       <Component {...pageProps} />
     </ThemeProvider>
   )
